1aef0c1c4a2955d6d3e376948df2f6285db1c488
DELEOAR <deleoar@oocl.com>
2018-07-30 14:01:57 +0800

    test02

---
+++ src/com/oocl/test02.java
@@ -0,0 +1,9 @@
+package com.oocl;
+
+public class test02 {
+
+    public static void main (String args[]) {
+        System.out.println("Hello there...");
+        System.out.println("Test no. 2");
+    }
+}
